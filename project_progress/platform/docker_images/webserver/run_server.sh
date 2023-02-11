e5b8f717599fe74a7ff841d81f3470602de7b537
Chris Papastamos <xristos.papastamos@gmail.com>
2023-02-11 23:20:55 +0200

    fixed run server

--- project_progress/platform/docker_images/webserver/run_server.sh
+++ project_progress/platform/docker_images/webserver/run_server.sh
@@ -1,7 +1,7 @@
 DIRECTORY='/home/chris/Classes/mini_internet_extention/project_progress/platform'
 DOCKERHUB_USER="miniinterneteth"
 
-docker build --tag="miniinterneteth/d_webserver" "."
+# docker build --tag="miniinterneteth/d_webserver" "."
 
 docker stop WEB
 docker rm WEB
