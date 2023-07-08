8785cb19545ebe8595cde61095064a2bc4978071
Chris Papastamos <xristos.papastamos@gmail.com>
2023-07-08 12:16:40 +0300

    added artemis go implementation

---
+++ artemis_go/cmd/root.go
@@ -0,0 +1,77 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/common-nighthawk/go-figure"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var UpdateFile string
+var PrefixFile string
+var HijackFile string
+var RelationsFile string
+var LineNo int64
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "ihd",
+	Short: "A BGP hijack detector",
+	Long:  `HCI is a BGP hijack detector witten in GO. It relies on historical data fetched with bgpreader and provided in a custom format.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&UpdateFile, "updates", "", "file containing bgp updates")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.PersistentFlags().StringVar(&PrefixFile, "prefixes", "", "file containing prefix-to-as mappings")
+	rootCmd.PersistentFlags().StringVar(&HijackFile, "output", "hijacks.csv", "file containing the detected hijacks")
+	rootCmd.PersistentFlags().Int64Var(&LineNo, "lineno", 0, "the number of lines the update file contains ")
+	rootCmd.PersistentFlags().StringVar(&RelationsFile, "relations", "", "CAIDA's graph for AS relationships ")
+
+	rootCmd.MarkPersistentFlagRequired("updates")
+	rootCmd.MarkPersistentFlagRequired("prefixes")
+	rootCmd.MarkPersistentFlagRequired("relations")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	myFigure := figure.NewFigure("IHD (INSPIRE Hijack Detector)", "", true)
+	myFigure.Print()
+
+}
+
---
+++ artemis_go/main.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/kentik/patricia"
+	"github.com/kentik/patricia/string_tree"
+	"github.com/schollz/progressbar/v3"
+	"github.com/shanmukhsista/cliapp/cmd"
+	"golang.org/x/exp/slices"
+)
+
+type HijackType string
+
+const (
+	SubPrefix   HijackType = "E|0|-|-"
+	ExactPrefix HijackType = "S|0|-|-"
+	Valid       HijackType = "valid"
+	Undefined   HijackType = "undefined"
+)
+
+type Hijack struct {
+	prefix          string
+	hijack_as       string
+	origin_as       string
+	hj_type         HijackType
+	time_started    float64
+	time_last       float64
+	time_ended      float64
+	peers_seen      []string
+	peers_withdrawn []string
+	state           HijackState
+	messageCount    int64
+}
+
+type BGPUpdate struct {
+	prefix      string
+	messageType string
+	origin_as   string
+	timestamp   float64
+	peer_as     string
+	path        string
+}
+
+type HijackState string
+
+const (
+	Ongoing   HijackState = "Ongoing"
+	Dormant   HijackState = "Dormant"
+	Withdrawn HijackState = "Withdrawn"
+)
+
+const messageCountThreshold = 2
+
+var ongoingHijackMap map[string]Hijack
+
+var detectedHijackMap map[string]Hijack
+
+var prefixMap map[string]string
+
+var prefixPeerMap map[string]string
+
+var withdrawalsMap map[string]Hijack
+
+func main() {
+	cmd.Execute()
+	if !cmd.CommandProvided {
+		os.Exit(1)
+	}
+	// Initialize hash tables
+	ongoingHijackMap = make(map[string]Hijack)
+	detectedHijackMap = make(map[string]Hijack)
+	prefixMap = make(map[string]string)
+	prefixPeerMap = make(map[string]string)
+	withdrawalsMap = make(map[string]Hijack)
+
+	//argCount := (len(os.Args) - 2) / 2
+	fmt.Println("")
+	fmt.Println("Generating Peer Graph...")
+	peerGraph := generatePeerGraph(cmd.RelationsFile)
+	//hijackFile := os.Args[len(os.Args)-1]
+
+	//for i := 0; i < argCount; i++ {
+	updatesFilename := cmd.UpdateFile
+	prefixMapFilename := cmd.PrefixFile
+
+	fmt.Println("Generating Patricia Tree...")
+	prefixASMap, prefixTree := generatePatriciaTree(prefixMapFilename)
+
+	fileUpdates, err := os.Open(updatesFilename) // "UPDATES_START_2021-11-12 00:00:00-END_2021-12-12 00:00:00.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(fileUpdates *os.File) {
+		err := fileUpdates.Close()
+		if err != nil {
+
+		}
+	}(fileUpdates)
+
+	csvReader2 := csv.NewReader(fileUpdates)
+	csvReader2.Comma = '|'
+	fmt.Println("Initiating Hijack Detection...")
+	bar := progressbar.Default(cmd.LineNo)
+	for {
+		updateRecord, err := csvReader2.Read()
+		if err == io.EOF {
+			break
+		}
+
+		var updateMessage BGPUpdate
+		updateMessage.prefix = updateRecord[0]
+		updateMessage.origin_as = updateRecord[1]
+		updateMessage.peer_as = updateRecord[2]
+		updateMessage.messageType = updateRecord[6]
+		bar.Add(1)
+		if len(updateRecord) < 9 {
+			continue
+		}
+		if s, err := strconv.ParseFloat(updateRecord[8], 64); err == nil {
+			updateMessage.timestamp = s
+		} else {
+			continue
+		}
+		updateMessage.path = updateRecord[3]
+
+		if strings.Contains(updateMessage.prefix, ":") || strings.Contains(updateRecord[1], "{") {
+			continue
+		}
+
+		hijackType, hijackerAs, _, updateMessage, asnOrigin, prefixMatched := getHijackDetectionStatus(updateMessage, prefixTree, prefixASMap, peerGraph)
+
+		if hijackType == Undefined {
+			continue
+		}
+
+		if updateMessage.messageType == "A" {
+			if hijackType != Valid {
+				handleAnnouncement(updateMessage, hijackType, hijackerAs, asnOrigin, prefixMatched)
+			}
+		} else { // Withdrawal message
+			handleWithdrawal(updateMessage, prefixMatched)
+		}
+	}
+	//}
+	printHijacks()
+}
+
+func handleWithdrawal(updateMessage BGPUpdate, prefixMatched string) {
+	markHijackTermination := func(hijackKey string, hijack Hijack) {
+		hijack.state = Withdrawn
+		hijack.time_ended = hijack.time_last
+		//withdrawalKey := fmt.Sprintf("%s_%s_%s", updateMessage.prefix, hijack_as, hijack_type)
+
+		withdrawalsMap[hijackKey] = hijack
+		if hijack.messageCount > messageCountThreshold {
+			detectedHijackMap[hijackKey+"_"+fmt.Sprintf("%f", hijack.time_started)] = hijack
+		}
+		delete(ongoingHijackMap, hijackKey)
+		delete(prefixMap, updateMessage.prefix)
+	}
+
+	hijackKey, prefixExists := prefixMap[prefixMatched]
+	_, keyExists := withdrawalsMap[hijackKey]
+
+	if prefixExists && !keyExists {
+		h := ongoingHijackMap[hijackKey]
+		if !slices.Contains(h.peers_withdrawn, updateMessage.peer_as) {
+			h.peers_withdrawn = append(h.peers_withdrawn, updateMessage.peer_as)
+			ongoingHijackMap[hijackKey] = h
+		}
+
+		if isSubset(h.peers_seen, h.peers_withdrawn) {
+			if getTimeDiffInSeconds(h.time_last, updateMessage.timestamp) > 600 {
+				markHijackTermination(hijackKey, h)
+			} else {
+				h.state = Withdrawn
+				if h.messageCount > messageCountThreshold {
+					detectedHijackMap[hijackKey+"_"+fmt.Sprintf("%f", h.time_started)] = h
+				}
+			}
+		}
+	}
+}
+
+func handleAnnouncement(updateMessage BGPUpdate, hijack_type HijackType, hijack_as string, asnOrigin string, prefixMatched string) {
+	hijackKey := fmt.Sprintf("%s_%s_%s", prefixMatched, updateMessage.origin_as, hijack_type)
+	hijackVal, ok := ongoingHijackMap[hijackKey]
+
+	var h Hijack
+	h.prefix = updateMessage.prefix
+	h.origin_as = updateMessage.origin_as
+	h.hj_type = hijack_type
+	h.hijack_as = hijack_as
+	timestamp := updateMessage.timestamp
+	h.time_ended = -1
+	h.time_started = timestamp
+	h.time_last = timestamp
+	h.peers_seen = hijackVal.peers_seen
+	h.peers_withdrawn = hijackVal.peers_withdrawn
+	h.messageCount = 1
+
+	prefixPeerMap[fmt.Sprintf("%s_%s", updateMessage.prefix, updateMessage.peer_as)] = updateMessage.prefix
+
+	if !slices.Contains(h.peers_seen, updateMessage.peer_as) {
+		h.peers_seen = append(h.peers_seen, updateMessage.peer_as)
+	}
+	// If the key exists
+	if ok {
+		h.messageCount = hijackVal.messageCount + 1
+		if isSubset(hijackVal.peers_seen, hijackVal.peers_withdrawn) {
+			if getTimeDiffInSeconds(hijackVal.time_last, timestamp) > 600 {
+				if hijackVal.messageCount > messageCountThreshold {
+					hijackVal.state = Withdrawn
+					hijackVal.time_ended = hijackVal.time_last + 600*1000
+					detectedHijackMap[hijackKey+"_"+fmt.Sprintf("%f", hijackVal.time_started)] = hijackVal
+				}
+				h.time_started = math.Min(float64(timestamp), float64(hijackVal.time_started))
+				h.time_last = math.Max(float64(float32(timestamp)), float64(hijackVal.time_last))
+				ongoingHijackMap[hijackKey] = h
+			} else {
+				h.state = Withdrawn
+				h.time_started = math.Min(float64(timestamp), float64(hijackVal.time_started))
+				h.time_last = math.Max(float64(float32(timestamp)), float64(hijackVal.time_last))
+				ongoingHijackMap[hijackKey] = h
+			}
+		} else {
+			h.state = Ongoing
+			h.time_started = math.Min(float64(timestamp), float64(hijackVal.time_started))
+			h.time_last = math.Max(float64(float32(timestamp)), float64(hijackVal.time_last))
+			ongoingHijackMap[hijackKey] = h
+		}
+	} else {
+		h.messageCount = 1
+		prefixMap[prefixMatched] = hijackKey
+		ongoingHijackMap[hijackKey] = h
+	}
+}
+
+func isOriginValid(validASes []string, updateMessage BGPUpdate) bool {
+	_, found := Find(validASes, updateMessage.origin_as)
+	return found
+}
+
+func getHijackDetectionStatus(updateMessage BGPUpdate, prefixTree *string_tree.TreeV4, prefixASMap map[string][]string, peerGraph map[string][]string) (HijackType, string, bool, BGPUpdate, string, string) {
+	var isPartialPrefixMatch bool
+	var treeMatchedASN string
+	var validPrefix string
+	var validOriginASNS []string
+
+	exactMatchASN, isExactPrefixMatch := prefixASMap[updateMessage.prefix]
+
+	hijackType := Valid
+	hijackerAs := "-1"
+	isHijack := false
+	v4IP, _, _ := patricia.ParseIPFromString(updateMessage.prefix)
+	isPartialPrefixMatch, treeMatchedASN = prefixTree.FindDeepestTag(*v4IP)
+
+	if !isPartialPrefixMatch {
+		hijackType = Undefined
+		return hijackType, hijackerAs, false, updateMessage, "", validPrefix
+	} else if isExactPrefixMatch && !isOriginValid(exactMatchASN, updateMessage) && len(updateMessage.path) != 0 {
+		hijackType = ExactPrefix
+		hijackerAs = updateMessage.origin_as
+		validPrefix = updateMessage.prefix
+		isHijack = true
+	} else if updateMessage.messageType == "W" {
+		if isPartialPrefixMatch && !isExactPrefixMatch {
+			validPrefix, validOriginASNS = splitPrefixASN(treeMatchedASN)
+
+			if !isOriginValid(validOriginASNS, updateMessage) {
+				peers, existsInPeerGraph := peerGraph[updateMessage.origin_as]
+				if existsInPeerGraph {
+					isPeer := false
+					for _, asn := range validOriginASNS {
+						_, isPeer = Find(peers, asn)
+						if isPeer {
+							break
+						}
+					}
+					if !isPeer {
+						hijackType = SubPrefix
+						hijackerAs = updateMessage.origin_as
+						isHijack = true
+					}
+				} else {
+					hijackType = SubPrefix
+					hijackerAs = updateMessage.origin_as
+					isHijack = true
+				}
+			}
+		}
+
+		if !isHijack {
+			superNet := getSuperNet(updateMessage.prefix).String()
+
+			_, okPref := prefixPeerMap[fmt.Sprintf("%s_%s", updateMessage.prefix, updateMessage.peer_as)]
+			_, okSuperPref := prefixPeerMap[fmt.Sprintf("%s_%s", superNet, updateMessage.peer_as)]
+
+			// implicit withdrawal
+			if okPref || okSuperPref {
+				updateMessage.messageType = "W"
+				if okSuperPref {
+					updateMessage.prefix = superNet
+					// delete(prefixPeerMap, fmt.Sprintf("%s_%s", sp, updateMessage.peer_as))
+				} else {
+					// delete(prefixPeerMap, fmt.Sprintf("%s_%s", updateMessage.prefix, updateMessage.peer_as))
+				}
+			}
+		}
+	} else {
+		if isPartialPrefixMatch && !isExactPrefixMatch {
+			hijackType = SubPrefix
+			hijackerAs = updateMessage.origin_as
+			isHijack = true
+		}
+	}
+
+	asnOrigin := ""
+	if isExactPrefixMatch {
+		asnOrigin = exactMatchASN[0]
+	} else {
+		if isPartialPrefixMatch {
+			asnOrigin = treeMatchedASN
+		}
+	}
+
+	return hijackType, hijackerAs, isHijack, updateMessage, asnOrigin, validPrefix
+}
+
+func printHijacks() {
+	fmt.Printf("We have detected %d hijacks\n", len(detectedHijackMap))
+	fmt.Printf("We have detected %d hijacks\n", len(ongoingHijackMap))
+	file, err := os.OpenFile(cmd.HijackFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	datawriter := bufio.NewWriter(file)
+	datawriter.WriteString("prefix,origin as,hijack type,hijacker asn,time started,time of last update,time ended,state,duration\n")
+
+	for _, hijack := range detectedHijackMap {
+		if hijack.state == Withdrawn {
+			hijack.time_ended = hijack.time_last
+		} else {
+			hijack.state = Dormant
+			hijack.time_ended = hijack.time_last + 600
+		}
+
+		hijackLine := fmt.Sprintf("%s,%s,%s,%s,%f,%f,%f,%s,%f,%d,%d,%d\n", hijack.prefix, hijack.origin_as, hijack.hj_type, hijack.hijack_as, hijack.time_started,
+			hijack.time_last, hijack.time_ended, hijack.state, getTimeDiffInSeconds(hijack.time_started, hijack.time_ended)/60, hijack.messageCount, hijack.peers_withdrawn, hijack.peers_seen)
+		_, _ = datawriter.WriteString(hijackLine)
+	}
+
+	for _, hijack := range ongoingHijackMap {
+		hijack.state = Dormant
+		hijack.time_ended = hijack.time_last + 600
+		if hijack.time_started != hijack.time_last {
+			hijackLine := fmt.Sprintf("%s,%s,%s,%s,%f,%f,%f,%s,%f,%d,%d,%d\n", hijack.prefix, hijack.origin_as, hijack.hj_type, hijack.hijack_as, hijack.time_started,
+				hijack.time_last, hijack.time_ended, hijack.state, getTimeDiffInSeconds(hijack.time_started, hijack.time_ended)/60, hijack.messageCount, hijack.peers_withdrawn, hijack.peers_seen)
+			_, _ = datawriter.WriteString(hijackLine)
+		}
+	}
+	datawriter.Flush()
+}
