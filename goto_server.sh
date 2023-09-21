456176a4b971d6fad8e6d2221f72ca8beffa2ea5
Chris Papastamos <xristos.papastamos@gmail.com>
2023-09-21 21:12:06 +0300

    minor fixes

--- goto_server.sh
+++ goto_server.sh
@@ -7,7 +7,11 @@ if [ -z "$as" ]; then
 	exit
 fi
 
-portn=$((2000+${as}))
-echo "Connecting to AS ${as}..."
-sshpass -p pastroumas ssh -o StrictHostKeyChecking=no -p ${portn} root@147.52.203.13
-exit
+if [ $1 = "server" ]; then
+	sshpass -p !hy335bhy335b! ssh -o StrictHostKeyChecking=no csduser01@147.52.203.13
+else
+	portn=$((2000+${as}))
+	echo "Connecting to AS ${as}..."
+	sshpass -p pastroumas ssh -o StrictHostKeyChecking=no -p ${portn} root@147.52.203.13
+	exit
+fi
