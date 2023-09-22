66e10ecd82ea04fcd9dd62ff3fea034d8961e230
Chris Papastamos <xristos.papastamos@gmail.com>
2023-09-22 16:43:55 +0300

    changed exabgp init

--- platform/setup/exabgp_setup.sh
+++ platform/setup/exabgp_setup.sh
@@ -56,118 +56,108 @@ done
 # Stop the script if there is no BGP_MONITOR container
 if [[ "$has_monitor" -eq 0 ]]; then
     echo "No BGP monitor specified, skipping exabgp_setup.sh"
-else
-    # create the config file
-    for as in ${as_array[@]}; do
-        echo "neighbor $(subnet_router_EXABGP_MONITOR "${as}" "neighbor" ) {
-	local-address $(subnet_router_EXABGP_MONITOR "${as}" "local-address" );
-    local-as 10000;
-    peer-as "${as}";
-    family {
-        ipv4 unicast;
+    exit 0
+fi
+
+# create the config file
+for as in ${as_array[@]}; do
+    echo "neighbor $(subnet_router_EXABGP_MONITOR "${as}" "neighbor" ) {
+local-address $(subnet_router_EXABGP_MONITOR "${as}" "local-address" );
+local-as 10000;
+peer-as "${as}";
+family {
+    ipv4 unicast;
+}
+api {
+    processes [ message-logger ];
+    receive {
+        parsed;
+        update;
     }
-    api {
-        processes [ message-logger ];
-        receive {
-            parsed;
-            update;
-        }
-    }
+}
 
 }
 " >> ${EXA_DIR}/exabgp.conf
-    
-    done
 
-    # start EXABGP_MONITOR container
-    docker run -itd --net='none' --name="EXABGP_MONITOR"  \
-        -v ${EXA_DIR}/exabgp.conf:/exabgp.conf \
-        -v ${EXA_DIR}/parser/parser.log:/parser/parser.log \
-        -v ${EXA_DIR}/parser/output.csv:/parser/output.csv \
-        --hostname="EXABGP_MONITOR" --cpus=2 --pids-limit 100 \
-        exabgp_monitor exabgp.conf
-    
-    # cache the docker pid for ovs-docker.sh
-    source ${DIRECTORY}/groups/docker_pid.map
-    DOCKER_TO_PID['EXABGP_MONITOR']=$(docker inspect -f '{{.State.Pid}}' EXABGP_MONITOR)
-    declare -p DOCKER_TO_PID > ${DIRECTORY}/groups/docker_pid.map
+done
 
-    passwd="$(openssl rand -hex 8)"
-    echo "${passwd}" >> "${DIRECTORY}"/groups/ssh_exabgp_monitor.txt
-    echo -e ""${passwd}"\n"${passwd}"" | docker exec -i EXABGP_MONITOR passwd root
-
-    subnet_ssh_exabgp_monitor="$(subnet_ext_sshContainer -1 "EXABGP_MONITOR")"
-    ./setup/ovs-docker.sh add-port ssh_to_group ssh_in EXABGP_MONITOR --ipaddress="${subnet_ssh_exabgp_monitor}"
-
-    echo -n "-- add-br exabgp_monitor " >> "${DIRECTORY}"/groups/add_bridges.sh
-    echo "ip link set dev exabgp_monitor up" >> "${DIRECTORY}"/groups/ip_setup.sh
-
-    has_gw=0    
-    for ((k=0;k<group_numbers;k++)); do
-        group_k=(${groups[$k]})
-        group_number="${group_k[0]}"
-        group_as="${group_k[1]}"
-        group_config="${group_k[2]}"
-        group_router_config="${group_k[3]}"
-
-        if [ "${group_as}" != "IXP" ];then
-
-            # Create as_prefixes.csv, format is: IP|AS|AS_NAME
-            echo "${group_number}.0.0.0/8|${group_number}|${group_number}" >> ${EXA_DIR}/as_prefixes.csv
-
-            readarray routers < "${DIRECTORY}"/config/$group_router_config
-            n_routers=${#routers[@]}
-
-            for ((i=0;i<n_routers;i++)); do
-                router_i=(${routers[$i]})
-                rname="${router_i[0]}"
-                property1="${router_i[1]}"
-
-                if [ "${property1}" = "BGP_MONITOR"  ];then
-                    subnet_bridge="$(subnet_router_EXABGP_MONITOR "${group_number}" "bridge")"
-                    subnet_exabgp_monitor="$(subnet_router_EXABGP_MONITOR "${group_number}" "monitor")"
-                    subnet_group="$(subnet_router_EXABGP_MONITOR "${group_number}" "group")"
-
-                    if [[ "$has_gw" -eq 0 ]]; then
-                        ./setup/ovs-docker.sh add-port exabgp_monitor group_"${group_number}" EXABGP_MONITOR --ipaddress="${subnet_exabgp_monitor}" --gateway="${subnet_bridge%.*}.1" --route="${group_number}.0.0.0/8"
-                        has_gw=1
-                    else
-                        ./setup/ovs-docker.sh add-port exabgp_monitor group_"${group_number}" EXABGP_MONITOR --ipaddress="${subnet_exabgp_monitor}" --route="${group_number}.0.0.0/8"
-                    fi
-
-                    mod=$((${group_number} % 100))
-                    div=$((${group_number} / 100))
-
-                    if [ $mod -lt 10 ];then
-                        mod="0"$mod
-                    fi
-                    if [ $div -lt 10 ];then
-                        div="0"$div
-                    fi
-
-                    ./setup/ovs-docker.sh add-port exabgp_monitor exabgp \
-                    "${group_number}"_"${rname}"router --ipaddress="${subnet_group}" \
-                    --macaddress="aa:22:22:22:"$div":"$mod
-
-                    ./setup/ovs-docker.sh connect-ports exabgp_monitor \
-                    group_"${group_number}" EXABGP_MONITOR \
-                    exabgp "${group_number}"_"${rname}"router
-                
-                # elif [ "${property1}" = "HIJACK"  ];then
-
-                #     echo -n "-- add-br hijack_lo " >> "${DIRECTORY}"/groups/add_bridges.sh
-                #     echo "ip link set dev hijack_lo up" >> "${DIRECTORY}"/groups/ip_setup.sh
-                                                    
-                #     ./setup/ovs-docker.sh add-port hijack_lo hijack \
-                #     "${group_number}"_"${rname}"router
-
-                #     ./setup/ovs-docker.sh connect-ports hijack_lo \
-                #     group_"${group_number}" hijack \
-                #     group_"${group_number}" hijack
+# start EXABGP_MONITOR container
+docker run -itd --net='none' --name="EXABGP_MONITOR"  \
+    -v ${EXA_DIR}/exabgp.conf:/exabgp.conf \
+    -v ${EXA_DIR}/parser/parser.log:/parser/parser.log \
+    -v ${EXA_DIR}/parser/output.csv:/parser/output.csv \
+    -e "CLEAR_TO_START=0" \
+    --hostname="EXABGP_MONITOR" --cpus=2 --pids-limit 100 \
+    exabgp_monitor /bin/bash
 
 
+# cache the docker pid for ovs-docker.sh
+source ${DIRECTORY}/groups/docker_pid.map
+DOCKER_TO_PID['EXABGP_MONITOR']=$(docker inspect -f '{{.State.Pid}}' EXABGP_MONITOR)
+declare -p DOCKER_TO_PID > ${DIRECTORY}/groups/docker_pid.map
+
+passwd="$(openssl rand -hex 8)"
+echo "${passwd}" >> "${DIRECTORY}"/groups/ssh_exabgp_monitor.txt
+echo -e ""${passwd}"\n"${passwd}"" | docker exec -i EXABGP_MONITOR passwd root
+
+subnet_ssh_exabgp_monitor="$(subnet_ext_sshContainer -1 "EXABGP_MONITOR")"
+./setup/ovs-docker.sh add-port ssh_to_group ssh_in EXABGP_MONITOR --ipaddress="${subnet_ssh_exabgp_monitor}"
+
+echo -n "-- add-br exabgp_monitor " >> "${DIRECTORY}"/groups/add_bridges.sh
+echo "ip link set dev exabgp_monitor up" >> "${DIRECTORY}"/groups/ip_setup.sh
+
+has_gw=0    
+for ((k=0;k<group_numbers;k++)); do
+    group_k=(${groups[$k]})
+    group_number="${group_k[0]}"
+    group_as="${group_k[1]}"
+    group_config="${group_k[2]}"
+    group_router_config="${group_k[3]}"
+
+    if [ "${group_as}" != "IXP" ];then
+
+        # Create as_prefixes.csv, format is: IP|AS|AS_NAME
+        echo "${group_number}.0.0.0/8|${group_number}|${group_number}" >> ${EXA_DIR}/as_prefixes.csv
+
+        readarray routers < "${DIRECTORY}"/config/$group_router_config
+        n_routers=${#routers[@]}
+
+        for ((i=0;i<n_routers;i++)); do
+            router_i=(${routers[$i]})
+            rname="${router_i[0]}"
+            property1="${router_i[1]}"
+
+            if [ "${property1}" = "BGP_MONITOR"  ];then
+                subnet_bridge="$(subnet_router_EXABGP_MONITOR "${group_number}" "bridge")"
+                subnet_exabgp_monitor="$(subnet_router_EXABGP_MONITOR "${group_number}" "monitor")"
+                subnet_group="$(subnet_router_EXABGP_MONITOR "${group_number}" "group")"
+
+                if [[ "$has_gw" -eq 0 ]]; then
+                    ./setup/ovs-docker.sh add-port exabgp_monitor group_"${group_number}" EXABGP_MONITOR --ipaddress="${subnet_exabgp_monitor}" --gateway="${subnet_bridge%.*}.1" --route="${group_number}.0.0.0/8"
+                    has_gw=1
+                else
+                    ./setup/ovs-docker.sh add-port exabgp_monitor group_"${group_number}" EXABGP_MONITOR --ipaddress="${subnet_exabgp_monitor}" --route="${group_number}.0.0.0/8"
                 fi
-            done
-        fi
-    done
-fi
+
+                mod=$((${group_number} % 100))
+                div=$((${group_number} / 100))
+
+                if [ $mod -lt 10 ];then
+                    mod="0"$mod
+                fi
+                if [ $div -lt 10 ];then
+                    div="0"$div
+                fi
+
+                ./setup/ovs-docker.sh add-port exabgp_monitor exabgp \
+                "${group_number}"_"${rname}"router --ipaddress="${subnet_group}" \
+                --macaddress="aa:22:22:22:"$div":"$mod
+
+                ./setup/ovs-docker.sh connect-ports exabgp_monitor \
+                group_"${group_number}" EXABGP_MONITOR \
+                exabgp "${group_number}"_"${rname}"router
+            
+            fi
+        done
+    fi
+done
