b498d5fab3d7586cdde32bbbf0eede3ba5eec9e7
Chris Papastamos <xristos.papastamos@gmail.com>
2023-09-23 19:36:18 +0300

    save the passwords to the text file as well

--- platform/utils/changeASpassword/changeASpass.sh
+++ platform/utils/changeASpassword/changeASpass.sh
@@ -1,4 +1,8 @@
 AS="$1"
 N_PASSWORD=$2 # new password
+
 echo -e ""${N_PASSWORD}"\n"${N_PASSWORD}"" | docker exec -i "${AS}"_ssh passwd root
 docker exec "${AS}"_ssh bash -c "kill -HUP \$(cat /var/run/sshd.pid)"
+
+# Change in password text file as well
+sed -i "s/${AS} .*/${AS} ${N_PASSWORD}/" /home/$(whoami)/mini_internet_extention/platform/groups/passwords.txt
