044b5b61b44876866fa8412db45894e452a9f9c0
Chris Papastamos <xristos.papastamos@gmail.com>
2023-10-03 18:37:23 +0300

    fixed change pass script

--- platform/utils/changeASpassword/changeASpass.sh
+++ platform/utils/changeASpassword/changeASpass.sh
@@ -1,8 +1,9 @@
 AS="$1"
 N_PASSWORD=$2 # new password
+DIRECTORY="$3"
 
 echo -e ""${N_PASSWORD}"\n"${N_PASSWORD}"" | docker exec -i "${AS}"_ssh passwd root
 docker exec "${AS}"_ssh bash -c "kill -HUP \$(cat /var/run/sshd.pid)"
 
 # Change in password text file as well
-sed -i "s/${AS} .*/${AS} ${N_PASSWORD}/" /home/$(whoami)/mini_internet_extention/platform/groups/passwords.txt
+sed -i "s/${AS} .*/${AS} ${N_PASSWORD}/" ${DIRECTORY}/groups/passwords.txt
