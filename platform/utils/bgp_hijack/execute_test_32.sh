5b7a403687fcea8daf797aa53ad40573679d484f
Chris Papastamos <xristos.papastamos@gmail.com>
2023-10-10 10:02:54 +0300

    fixed typos

--- platform/utils/bgp_hijack/execute_test_32.sh
+++ platform/utils/bgp_hijack/execute_test_32.sh
@@ -8,7 +8,7 @@ mkdir tests
 for attacker in "${targets[@]}"
 do
     echo "Executing hijack.sh for AS$target"
-    ./hijack.sh -a $attacker -p $((attacker+1)).0.0.0/8 -r WEST -t 60 > tests/test_$attacker.log &
+    /bin/bash $(pwd)/hijack.sh -a $attacker -p $((attacker+1)).0.0.0/8 -r WEST -t 60 > tests/test_$attacker.log &
     # Sleep for 15 min
     sleep 900
 done
