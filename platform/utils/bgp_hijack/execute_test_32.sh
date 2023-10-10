ac7e6a5e5e1f76a5cf4365c0b269e76a416f22fe
Chris Papastamos <xristos.papastamos@gmail.com>
2023-10-10 14:15:37 +0300

    added stress test

--- platform/utils/bgp_hijack/execute_test_32.sh
+++ platform/utils/bgp_hijack/execute_test_32.sh
@@ -5,19 +5,53 @@ if (($UID != 0)); then
     exit
 fi
 
+# Parse command line arguments
+while getopts ":normal:stress" opt; do
+    case $opt in
+        normal)
+            type="normal"
+            ;;
+        stress)
+            type="stress"
+            ;;
+        \?)
+            type="all"
+            ;;
+    esac
+done
+
 # List of targets
-targets=(1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31)
 
 rm -rf tests | True > /dev/null 2>&1
 mkdir tests | True > /dev/null 2>&1
 
-# Loop through each target and execute hijack.sh
-for attacker in "${targets[@]}"
-do
-    echo "Executing hijack.sh for AS${target}"
-    /bin/bash $(pwd)/hijack.sh -a ${attacker} -p $((attacker+1)).0.0.0/8 -r EAST -t 60 > tests/test_${attacker}.log 2>&1 &
+# Execute tests
+if [ "$type" = "normal" ] | [ "$type" = "all" ]; then
+    # Execute normal tests
+    echo "Executing normal tests"
+    targets=(1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31)
+    for attacker in "${targets[@]}"
+    do
+        echo "Executing hijack.sh for AS${attacker}"
+        /bin/bash $(pwd)/hijack.sh -a ${attacker} -p $((attacker+1)).0.0.0/8 -r EAST -t 60 > tests/test_${attacker}.log 2>&1 &
 
-    echo "Sleeping for 15 minutes"
-    sleep 900
-done
+        echo "Sleeping for 15 minutes"
+        sleep 900
+    done
+fi
+if [ "$type" = "stress" ] | [ "$type" = "all" ]; then
+    # Execute stress test
+    echo "Executing stress tests"
+    targets=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 \
+             18 19 20 21 22 23 24 25 26 27 28 29 30 31 32)
+    for attacker in "${targets[@]}"
+    do
+        echo "Executing hijack.sh for AS${attacker}"
+        if [ "$attacker" -eq 32 ]; then
+            /bin/bash $(pwd)/hijack.sh -a ${attacker} -p 1.0.0.0/8 -r EAST -t 60 > tests/test_${attacker}.log 2>&1 &
+        else
+            /bin/bash $(pwd)/hijack.sh -a ${attacker} -p $((attacker+1)).0.0.0/8 -r EAST -t 60 > tests/test_${attacker}.log 2>&1 &
+        fi
+    done
+fi
 
