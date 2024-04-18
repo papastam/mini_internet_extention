eeabb317df6a15ad6fa50a583dc54d86cd341907
Chris Papastamos <xristos.papastamos@gmail.com>
2024-04-18 13:05:46 +0300

    removed not used vars

--- platform/setup/router_config.sh
+++ platform/setup/router_config.sh
@@ -14,15 +14,9 @@ source "${DIRECTORY}"/setup/_parallel_helper.sh
 # read configs
 readarray groups < "${DIRECTORY}"/config/AS_config.txt
 readarray extern_links < "${DIRECTORY}"/config/aslevel_links.txt
-readarray l2_switches < "${DIRECTORY}"/config/l2_switches.txt
-readarray l2_links < "${DIRECTORY}"/config/l2_links.txt
-readarray l2_hosts < "${DIRECTORY}"/config/l2_hosts.txt
 
 group_numbers=${#groups[@]}
 n_extern_links=${#extern_links[@]}
-n_l2_switches=${#l2_switches[@]}
-n_l2_links=${#l2_links[@]}
-n_l2_hosts=${#l2_hosts[@]}
 
 # create initial configuration for each router
 for ((k=0;k<group_numbers;k++));do
